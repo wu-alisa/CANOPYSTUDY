65e7cfbfcf57d8433cade19fd5ff0c381ef65381
Alisa Wu <alisawu.syd@gmail.com>
2022-08-18 21:08:00 +1000

    finished

--- Bundle.java
+++ Bundle.java
@@ -1,9 +1,10 @@
 import java.util.HashMap;
+import java.lang.Math;
 
 public class Bundle { 
 
     static String learnModeAlgorithm(Question[] bundleQuestions, QuestionResponse[] bundleResponses) { 
-        // a map with keys corresponding the question id's and how many times student got it right 
+        // a map with: keys corresponding the question id's, values corresponding to how many times student got it right 
         HashMap<String, Integer> allMarks = new HashMap<String, Integer>();
         for (Question q: bundleQuestions) { 
             allMarks.put(q.id, 0);
@@ -17,32 +18,48 @@ public class Bundle {
         }
 
         while (true) { 
-            // get the question id corresnponding to the (or one of the) smallest values. 
+            // get the question id corresponding to the (or one of the) smallest values. 
             String minId = null; 
             Integer minValue = null; 
+            int incomplete = 0; // the number of incomplete questions 
+            
             for (HashMap.Entry<String, Integer> entry : allMarks.entrySet()) {
                 String key = entry.getKey();
                 Integer value = entry.getValue();
+                // a question is considered incomplete if student got it right less than 2 times
+                if (value < 2) { 
+                    incomplete++; 
+                }
                 if (minValue == null || minValue > value) { 
                     minValue = value; 
                     minId = key; 
                 }
             }
-            // if min is 2, return null
+            // if min is 2, this means all questions have been completed, return null
             if (minValue == 2) { 
                 return null; 
             }
 
-            // if all other questions have reached 2 points, then return this question id
+            // if there is only 1 incomplete question left, then return this question id
+            if (incomplete == 1) { 
+                return minId; 
+            }
 
-            // if the question was shown once already in the 2nd half of bundleResponses array, add 1 value to it and continue 
+            // if the question was shown recently (if in the 2nd half of bundleResponses array), increase its map value by 1 and continue 
+            for (int i = Math.round(bundleResponses.length / 2); i < bundleResponses.length; i++) {
+                if (bundleResponses[i].questionId.equals(minId)) { 
+                    allMarks.put(minId, allMarks.get(minId) + 1); 
+                    continue; 
+                }
+            }
 
             // all else, return this id 
+            return minId; 
         }
 
 }
 
     public static void main(String[] args) { 
-        // clarify that bundleResponses is in onder (index 0 is oldest answer, last index is most recent answer )
+
     }
 }
